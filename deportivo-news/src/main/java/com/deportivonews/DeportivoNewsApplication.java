c67df1b7e3675572c457fd3e71ca27d8e0fd9e4a
afdelgadoisc <afdelgadoisc@gmail.com>
2023-08-14 18:13:04 -0500

    initial project

---
+++ deportivo-news/src/main/java/com/deportivonews/DeportivoNewsApplication.java
@@ -0,0 +1,13 @@
+package com.deportivonews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeportivoNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeportivoNewsApplication.class, args);
+	}
+
+}
---
+++ deportivo-news/src/test/java/com/deportivonews/DeportivoNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deportivonews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeportivoNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
